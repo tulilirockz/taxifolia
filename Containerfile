bb51cc7f30d30016cd52be0cf10993b63cc21954
Tulip Blossom <tulilirockz@proton.me>
2025-08-26 22:03:07 -0300

    fix: lint failing because of /var/roothome
    Signed-off-by: Tulip Blossom <tulilirockz@proton.me>

--- Containerfile
+++ Containerfile
@@ -11,4 +11,4 @@ RUN --mount=type=tmpfs,dst=/var \
     --mount=type=bind,from=ctx,source=/,dst=/tmp/build-scripts \
     /tmp/build-scripts/build.sh
 
-RUN bootc container lint --fatal-warnings
+RUN rm -rf /var/* && bootc container lint --fatal-warnings
--- build.sh
+++ build.sh
@@ -75,7 +75,3 @@ KERNEL_VERSION="$(find "/usr/lib/modules" -maxdepth 1 -type d ! -path "/usr/lib/
 export DRACUT_NO_XATTR=1
 dracut --no-hostonly --kver "$KERNEL_VERSION" --reproducible --zstd -v --add ostree -f "/usr/lib/modules/$KERNEL_VERSION/initramfs.img"
 chmod 0600 "/usr/lib/modules/${KERNEL_VERSION}/initramfs.img"
-
-rm -rf /var/*
-
-bootc container lint
