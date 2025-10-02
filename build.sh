2b890d5a861f8c28d207b82b9bb8c878790017a9
Tulip Blossom <tulilirockz@posteo.com>
2025-10-02 08:34:07 -0300

    chore: add sysctl rules for unprivileged unbound
    Signed-off-by: Tulip Blossom <tulilirockz@posteo.com>

--- build.sh
+++ build.sh
@@ -71,10 +71,17 @@ tee /usr/lib/systemd/zram-generator.conf <<EOF
 zram-size = min(ram, 8192)
 EOF
 
+tee /usr/lib/sysctl.d/99-rcore-memmax.conf <<EOF
+# Required for unprivileged unbound
+net.core.rmem_max=262144
+EOF
+
 sed -i 's|^ExecStart=.*|ExecStart=/usr/bin/bootc update --quiet|' /usr/lib/systemd/system/bootc-fetch-apply-updates.service
 sed -i 's|^OnUnitInactiveSec=.*|OnUnitInactiveSec=7d\nPersistent=true|' /usr/lib/systemd/system/bootc-fetch-apply-updates.timer
 sed -i 's|#AutomaticUpdatePolicy.*|AutomaticUpdatePolicy=stage|' /etc/rpm-ostreed.conf
 sed -i 's|#LockLayering.*|LockLayering=true|' /etc/rpm-ostreed.conf
+
+systemctl enable bootc-fetch-apply-updates
 
 cat >/usr/lib/systemd/system-preset/91-resolved-default.preset <<'EOF'
 enable systemd-resolved.service
